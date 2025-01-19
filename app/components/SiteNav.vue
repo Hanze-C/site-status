f6e67c59541044f4cad377bdcaefb3fab61162f8
Hanze-C <hanze2010@outlook.com>
2025-01-19 23:13:04 +0800

    Update SiteNav.vue

--- app/components/SiteNav.vue
+++ app/components/SiteNav.vue
@@ -89,8 +89,6 @@ const navMenu = computed<DropdownOption[]>(() => [
     key: "about",
     label: t("nav.about"),
     icon: renderIcon("icon:info"),
-    props: {
-      onClick: () => window.open("https://notice.hanze.icu"),
   },
   {
     key: "logout",
